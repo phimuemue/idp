3282461bbb492329748855f01667eed5c4937f48
phimuemue <descpl@yahoo.de>
2012-06-26 16:11:05 +0300

    Update master

--- matrix_constructor.py
+++ matrix_constructor.py
@@ -3,13 +3,34 @@
 f=open("matrices.h","w")
 
 import glob, os
+import re
 
 for mat in glob.glob("./*.matrix"):
     print ("Processing " +  mat + " ...")
     f.write ("T " + os.path.basename(mat).split(".matrix")[0] + " = {\n")
+    # collect stuff from file
+    contents = []
     for line in open(mat):
-        f.write("\t{" + line.strip() + "},\n")
+        if line.lower().startswith("warning") or line.startswith(">"):
+            continue
+        parts = re.match(".*?(\[.*\]).*=(.*);",line).groups()
+        index = [int(x) for x in re.search("\[(.*?)\]\[(.*)\]",parts[0]).groups()]
+        value = parts[1]
+        contents.append(index+[value,])
+    # sort stuff
+    maxx = max(contents, key=lambda x:x[0])[0]
+    maxy = max(contents, key=lambda x:x[1])[1]
+    print maxx
+    print maxy
+    realcontents = [[0 for y in xrange(maxy+1)] for x in xrange(maxx+1)]
+    for [x,y,c] in contents:
+        realcontents[x][y] = c
+    # write stuff to matrices.h
+    for i in realcontents:
+        f.write("\t{" + ",".join(i) + "},\n")
+
     f.write("};\n")
 f.close()
+
 
 
